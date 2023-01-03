9e528a296b2c9503ce808b3e78ef2668c0de53f5
Bruno Gusmão <brunogusmao43@gmail.com>
2023-01-03 18:10:51 -0300

    preparing the base of the project

--- PizzaDev/src/app/app-routing.module.ts
+++ PizzaDev/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 const routes: Routes = [];
 
