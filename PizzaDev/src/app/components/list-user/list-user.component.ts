42319178158e4e0c78bde08186693aa54c0ff1b3
Bruno Gusmão <brunogusmao43@gmail.com>
2023-02-14 15:52:01 -0300

    implementing the TS logic to the list user component

--- PizzaDev/src/app/components/list-user/list-user.component.ts
+++ PizzaDev/src/app/components/list-user/list-user.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Inject } from '@angular/core';
+import {Router} from "@angular/router";
+import { data } from 'jquery';
+import {ApiService} from '../api-service/api-service.component';
+
 
 @Component({
   selector: 'app-list-user',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class ListUserComponent {
 
+  constructor(private router: Router, private apiService: ApiService) { }
+
+  ngOnInit() {
+    if(!window.localStorage.getItem('token')) {
+      this.router.navigate(['login']);
+      return;
+    }
+    this.apiService.getUsers().subscribe(data => {
+      this.users = data.result;
+    });
+  }
+
+  deleteUser(userId: string) {
+    this.apiService.deleteUser(userId).subscribe(data => {
+      this.users = this.users.filter(u => u !== user);
+    })
+  };
+  editUser(userId: string): void {
+    window.localStorage.removeItem("editUserId");
+    window.localStorage.setItem("editUserId", user.id.toString(userId));
+    this.router.navigate(['add-user']);
+  };
+
+  addUser(): void {
+    this.router.navigate(['add-user']);
+  };
 }
