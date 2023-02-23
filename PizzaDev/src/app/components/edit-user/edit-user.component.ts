210401d58ab13967c84b150db3299307c77ddae5
Bruno Gusmão <brunogusmao43@gmail.com>
2023-02-23 15:44:15 -0300

    on the TS logic, making a making a API call to fetch user by user Id

--- PizzaDev/src/app/components/edit-user/edit-user.component.ts
+++ PizzaDev/src/app/components/edit-user/edit-user.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {first} from "rxjs/operators";
+import {ApiService} from "../../service/api.service";
+
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.css']
 })
-export class EditUserComponent {
+export class EditUserComponent implements OnInit {
+  user: User;
+  editForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private router: Router, private apiService) { }
 
+  ngOnInit() {
+    let userId = window.localStorage.getItem("editUserId");
+    if(!userId) {
+      alert('Invalid Action!');
+      this.router.navigate(['list']);
+      return;
+    }
+
+    this.editForm = this.formBuilder.group({
+      id: [''],
+      username: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      age: ['', Validators.required]
+    });
+    this.apiService.getUserById(+userId).subscribe( data => {
+      this.editForm.setValue(data.result);
+    });
+
+  }
+
+  onSubmit(): void {
+    
+  }
 }
